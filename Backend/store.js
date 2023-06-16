67c228ee07ba3d8b29c3f2f41dd3b60cd5b15701
TranDat <dattt@mor.com.vn>
2023-06-16 16:14:18 +0700

    remove some file

--- Backend/store.js
+++
@@ -1,8 +0,0 @@
-import { userReducer } from "./reducers/userReducer"
-import {configureStore} from '@reduxjs/toolkit'
-export const store = configureStore({
-    reducer:{
-        user:userReducer
-    }
-})
-export const server = "https://myshop-11m9.onrender.com/api/v1"
